246f74fa3fa9d524bd3c0dccc512df0bc4331c29
Bryson Choy <bryson.choy@ucsf.edu>
2023-02-10 00:53:28 -0800

    fixed transcribe tests

--- tests/test_transcribe.py
+++ tests/test_transcribe.py
@@ -26,9 +26,29 @@ def test_transcribe():
     Write your unit test for the
     transcribe function here.
     """
-    assert transcribe("AGGCTAGTACTGC", reverse=False) == "AGGCUAGUACUGC"
-    self.assertRaises(ValueError, transcribe(""))
-    self.assertRaises(ValueError, transcribe("ERGHO"))
+    assert transcribe("AGGCTAGTACTGC", reverse=False) == "AGGCUAGUACUGC", "Transcribe function does not function properly"
+
+    try:
+        empty_str = transcribe("", reverse=False)
+        test_empty_str = False
+    except ValueError:
+        test_empty_str = True
+
+    try:
+        bad_str = transcribe("UCSF", reverse=False)
+        test_bad_str = False
+    except ValueError:
+        test_bad_str = True
+
+    try:
+        no_str = transcribe(0, reverse=False)
+        test_no_str = False
+    except ValueError:
+        test_no_str = True
+
+    assert test_empty_str = True, "Input to transcribe cannot be an empty string"
+    assert test_bad_str = True, "Input to transcribe must contain AGCT"
+    assert test_no_str = True, "Input to transcribe must be a string"
 
 
 def test_reverse_transcribe():
@@ -36,7 +56,27 @@ def test_reverse_transcribe():
     Write your unit test for the
     reverse transcribe function here.
     """
-    seq = "AGGCTAGTACTGC"
-    assert reverse_transcribe(seq) == "UCCGAUCAUGACG"
-    self.assertRaises(ValueError, reverse_transcribe(""))
-    self.assertRaises(ValueError, transcribe("ERGHO"))
+    assert reverse_transcribe("AGGCTAGTACTGC") == "UCCGAUCAUGACG", "Reverse transcribe function does not function properly"
+    
+    try:
+        empty_str = reverse_transcribe("")
+        test_empty_str = False
+    except ValueError:
+        test_empty_str = True
+
+    try:
+        bad_str = reverse_transcribe("UCSF")
+        test_bad_str = False
+    except ValueError:
+        test_bad_str = True
+
+    try:
+        no_str = reverse_transcribe(0)
+        test_no_str = False
+    except ValueError:
+        test_no_str = True
+
+    assert test_empty_str = True, "Input to reverse_transcribe cannot be an empty string"
+    assert test_bad_str = True, "Input to reverse_transcribe must contain AGCT"
+    assert test_no_str = True, "Input to reverse_transcribe must be a string"
+
